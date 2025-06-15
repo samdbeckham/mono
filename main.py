8bd7823bd9afb431a153b9c3bba81309c7ffafdf
samdbeckham <sam@beckham.io>
2025-06-15 22:21:28 +0100

    Adds a schema for getting files

--- main.py
+++ main.py
@@ -3,6 +3,17 @@ import sys
 from dotenv import load_dotenv
 from google import genai
 from google.genai import types
+from available_functions import available_functions
+
+system_prompt = """
+You are a helpful AI coding agent.
+
+When a user asks a question or makes a request, make a function call plan. You can perform the following operations:
+
+- List files and directories
+
+All paths you provide should be relative to the working directory. You do not need to specify the working directory in your function calls as it is automatically injected for security reasons.
+"""
 
 def main():
     load_dotenv()
@@ -15,12 +26,20 @@ def generate_content(client, user_prompt):
     ]
     response = client.models.generate_content(
         model="gemini-2.0-flash-001",
-        contents=messages
+        contents=messages,
+        config=types.GenerateContentConfig(
+            system_instruction=[system_prompt],
+            tools=[available_functions]
+        )
     )
     log(response, user_prompt, "--verbose" in sys.argv)
 
 def log(response, user_prompt, verbose):
-    print(response.text)
+    if response.function_calls:
+        for call in response.function_calls:
+            print(f"Calling function: {call.name}({call.args})")
+    else:
+        print(response.text)
     if (verbose):
         print(f"User prompt: {user_prompt}")
         print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
