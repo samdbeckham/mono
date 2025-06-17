7c1083c2fe5c4f4c6600dca5c8109f563d284fec
samdbeckham <sam@beckham.io>
2025-06-17 13:52:37 +0100

    Adds better default for get_file_info directory

--- functions/get_files_info.py
+++ functions/get_files_info.py
@@ -1,7 +1,7 @@
 import os
 
-def get_files_info(working_directory, directory="."):
-    target_directory = os.path.join(working_directory, directory)
+def get_files_info(working_directory, directory=None):
+    target_directory = os.path.join(working_directory, directory) if directory is not None else working_directory
     if not os.path.abspath(target_directory).startswith(os.path.abspath(working_directory)):
         return f'Error: Cannot list "{directory}" as it is outside the permitted working directory'
     if not os.path.isdir(target_directory):
